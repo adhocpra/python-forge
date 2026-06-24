5530a190d248fb69ef49cdefa6501be7ce969e89
adhocpra <adhpravat12@gmail.com>
2026-06-23 22:28:40 -0500

    Add file handling basics: read, write, append, readline, and context manager patterns

---
+++ OOP/9-File-Handling/basic/proper_open_close.py
@@ -0,0 +1,7 @@
+file= open("write1.tx","w")
+
+file.write("this is a new file\n")
+file.write("this is the second line\n")
+file.write("\nthis is third line\n")
+
+file.close()
---
+++ OOP/9-File-Handling/basic/writing.py
@@ -0,0 +1,12 @@
+
+#file =open("filename.txt", "mode")
+
+file =open("write.txt", "w")
+
+#using write() method to write
+#file.write("This is the first txt file")
+file.write("this is the changed text")
+
+#file should be closed after working
+file.close()
+file= open("write1.txt", "w")
