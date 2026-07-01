9bb6bb0eaa5ee2bd50a8be16bd5b4afbc3548b1b
adhocpra <adhpravat12@gmail.com>
2026-07-01 13:41:22 -0500

    Add Enhanced folder with lambda functions

---
+++ Enhanced/lamda functions.py
@@ -0,0 +1,11 @@
+add= lambda a,b:a+b
+print (add(3,5))
+
+square= lambda x:x**2
+print(square(4))
+
+greet= lambda name: "Hello "  + name
+print(greet("ram"))
+
+is_even= lambda x: x%2==0
+print(is_even(4))
