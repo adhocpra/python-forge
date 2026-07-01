dcbdd8a5819e75f03f409a3b669aed5e18f39a5f
adhocpra <adhpravat12@gmail.com>
2026-07-01 14:30:37 -0500

    Remove Gemini-Chatbot duplicate

--- Gemini-Chatbot/01_get_request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-url= "https://api.github.com//users/adhocpra"
-
-response= requests.get(url)
-print("status code:" , response.status_code)
-print("Response JSON:", response.json())
--- Gemini-Chatbot/04_error_handling.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response= requests.get("https://api.github.com/users/thisuserdoesnotexist999")
-if response.status_code ==200:
-    print(response.json())
-elif response.status_code==404:
-    print("User not found")
-else:
-    response.raise_for_status()
